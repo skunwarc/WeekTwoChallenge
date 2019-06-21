f4a14815e4be6b6ff3d491ee8198ff066b9cc63b
skunwarc <sagar.kunwar24@gmail.com>
2019-06-21 13:28:05 -0400

    second commit

--- src/com/mc/week2/Chipotle.java
+++ src/com/mc/week2/Chipotle.java
@@ -1,29 +1,54 @@
 package com.mc.week2;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Chipotle {
-
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
         Random rn = new Random();
-        String[] burritos = new String[25];
-        ArrayList<String> ingredients = new ArrayList<>();
-        ingredients.add("Rice");
-        ingredients.add("Meat");
-        ingredients.add("Beans");
-        ingredients.add("Salsa");
-        ingredients.add("Veggies");
 
-        //Collections.addAll(ingredients,"white, brown, none","chicken, steak,carnidas,chorizo, sofritas, veggies",
-        // "pinto, black, none","mild, medium,hot, none, all","lettuce,fajita veggies, none, all");
+        List<String> rice = new ArrayList<>();
+        rice .add("white rice ");
+        rice .add("brown rice ");
+        rice .add("no rice");
+        int riceRand= new Random().nextInt(3);
+        String riceGen = rice.get(riceRand);
 
-        for (int i=1;i <=burritos.length;i++){
-            System.out.println("Burrito " + i+ ": "+ ingredients.get(rn.nextInt(ingredients.size())));
+        List<String> meat = new ArrayList<>();
+        meat.add("chicken");
+        meat.add("steak");
+        meat.add("carnidas");
+        meat.add("chorizo");
+        meat.add("sofritas");
+        meat.add("veggies");
+        int meatRand = new Random().nextInt(6);
+        String meatGen = meat.get(meatRand);
+
+        List<String> beans = new ArrayList<>();
+        beans.add("pinto");
+        beans.add("black");
+        beans.add("no beans");
+        int beansRand = rn.nextInt(3);
+
+        List<String> salsa = new ArrayList<>();
+        salsa.add("mild salsa");
+        salsa.add("medium salsa");
+        salsa.add("hot salsa");
+        salsa.add("no");
+        salsa.add("mild+medium+hot salsa");
+        int salsaRand = rn.nextInt(5);
+
+        List<String> veggies = new ArrayList<>();
+        veggies.add("lettuce");
+        veggies.add("fajita");
+        veggies.add("lettuce+fajita+ vaggies");
+        veggies.add("no");
+        int veggiesRand = rn.nextInt(4);
+
+        for (int i =0; i<=25; i++){
+
+            System.out.println("Burrito "+ i+ ": "+ riceGen+ ", "+ meatGen+", ");
         }
+
+
     }
 }
